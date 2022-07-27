e4883f74dadbc3f905847965a480023e83e4f50e
SOLBI1028 <hagkthfql@naver.com>
2022-07-27 19:41:23 +0900

    feat(fe): set routing path

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,13 +4,16 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import Router from './Router';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>
 );
 
