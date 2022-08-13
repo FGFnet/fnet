ffe56798465083b8a2b665a6e695b9ffbf818f4b
SOLBI1028 <hagkthfql@naver.com>
2022-08-13 18:55:48 +0900

    feat(fe): create home screen

--- frontend/src/screen/home/home.tsx
+++ frontend/src/screen/home/home.tsx
@@ -1,10 +1,38 @@
 import React from 'react'
+import { Colors } from '../../constant'
+import Banner from './component/Banner'
+import ScheduleSection from './component/scheduleSection'
+import FGSection from './component/fgSection'
+import { FiSettings } from 'react-icons/fi'
+import {Grid} from '@mui/material'
 
 export default function HomeScreen() {
+  const boxStyle = {
+    padding: 40,
+    boxShadow: '0px 4px 12px rgba(58,58,58,0.25)',
+    borderRadius: 20,
+    height: '100%',
+    marginBottom: 20
+  }
+
   return (
-    <div>
-      <h1>Home 하솔비</h1>
-      Dashboard
-    </div>
+    <React.Fragment>
+      <Banner />
+      <Grid container wrap="wrap" justifyContent="space-around">
+        <Grid item md={4} style={boxStyle}>
+          <FGSection />
+        </Grid>
+        <Grid item md={6} sm={10} style={boxStyle}>
+          <div style={{fontSize: 20, textAlign: 'center', color: Colors.primary, fontWeight: 'bold', marginBottom: 20,}}>
+            Team-Building Schedule <FiSettings/>
+          </div>
+          <div style={{textAlign: 'center'}}>
+            <ScheduleSection date="2023.02.15" lc={9}/>
+            <ScheduleSection date="2023.02.16" lc={16}/>
+            <ScheduleSection date="2023.02.17"/>
+          </div>
+        </Grid>
+      </Grid>
+    </React.Fragment>
   )
 }
