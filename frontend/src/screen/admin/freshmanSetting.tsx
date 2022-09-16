74065c1be043fcc3ab3a55d22de9c787efcda09d
kig_laptop_2 <idmatthew9546@gmail.com>
2022-09-16 14:55:33 +0900

    feat(fe): create freshmanSetting screen

--- frontend/src/screen/admin/freshmanSetting.tsx
+++ frontend/src/screen/admin/freshmanSetting.tsx
@@ -1,9 +1,149 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Container, Grid, Box, Button, Divider } from '@mui/material'
+import { MenuButton, AdminTable, Loading } from './component'
+import { Header, Title } from '../../component'
 
-export default function FreshmanSettingScreen() {
+const data = [
+  {
+    name: '김일건',
+    phone_number: '010-1234-5678',
+    lc: 'LC08',
+    register: true,
+  },
+  {
+    name: '김일건1',
+    phone_number: '010-1234-5678',
+    lc: 'LC08',
+    register: true,
+  },
+  {
+    name: '김일건2',
+    phone_number: '010-1234-5678',
+    lc: 'LC08',
+    register: true,
+  },
+  {
+    name: '김일건3',
+    phone_number: '010-1234-5678',
+    lc: 'LC08',
+    register: true,
+  },
+  {
+    name: '김일건4',
+    phone_number: '010-1234-5678',
+    lc: 'LC08',
+    register: true,
+  },
+  {
+    name: '김일건5',
+    phone_number: '010-1234-5678',
+    lc: 'LC08',
+    register: true,
+  },
+  {
+    name: '김일건6',
+    phone_number: '010-1234-5678',
+    lc: 'LC08',
+    register: true,
+  },
+  {
+    name: '김일건7',
+    phone_number: '010-1234-5678',
+    lc: 'LC08',
+    register: true,
+  },
+  {
+    name: '김일건8',
+    phone_number: '010-1234-5678',
+    lc: 'LC08',
+    register: true,
+  },
+  {
+    name: '김일건9',
+    phone_number: '010-1234-5678',
+    lc: 'LC08',
+    register: true,
+  },
+  {
+    name: '김일건10',
+    phone_number: '010-1234-5678',
+    lc: 'LC08',
+    register: true,
+  },
+]
+
+export default function FgSettingScreen() {
+  const [tableData, updateTableData] = useState([])
+  const [loading, setLoading] = useState(false)
+
+  const tableColumn = [
+    { id: 'index', label: '#' },
+    { id: 'name', label: '이름' },
+    { id: 'phone_number', label: '전화번호' },
+    { id: 'lc', label: 'LC' },
+    { id: 'register', label: '등록' },
+  ]
+
+  // api 작동 확인 필요
+  /*
+  const fetchUsers = async () => {
+    try {
+      setLoading(true)
+      const res = await api.getFreshmanList()
+      updateTableData(res.data.data)
+    } catch (err) {
+      alert(err)
+      setLoading(false)
+    }
+  }
+  */
+
+  const uploadFile = async (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files != null) {
+      setLoading(true)
+      // api 작동 확인 필요
+      /*
+      const formData = new FormData()
+      formData.append('file', event.target.files[0])
+      try {
+        const res = await api.uploadFreshman(formData);
+        if (!res.data.error) {
+          alert('Upload Successful');
+          setSingleFile(null)
+          await fetchUsers()
+        }
+      } catch (err) {
+        alert(err)
+      } finally{
+
+      }
+      */
+      setTimeout(() => {
+        setLoading(false)
+      }, 500)
+    } else {
+      alert('파일이 선택되지 않았습니다.')
+    }
+  }
+
   return (
-    <div>
-      <h1>freshman setting</h1>
-    </div>
+    <React.Fragment>
+      <Container maxWidth="lg">
+        <Header title="설정" children={<MenuButton />} />
+        <Grid mt={1} mb={1}>
+          <Title title="신입생 명단" background="#D0DBCC" />
+          <Box component="span" sx={{ float: 'right' }}>
+            <Button variant="outlined" component="label">
+              Upload File
+              <input type="file" accept=".xlsx, .xls" hidden onChange={uploadFile} />
+            </Button>
+          </Box>
+        </Grid>
+        <Divider />
+        <Grid container mt={1} mb={1}>
+          {loading ? <Loading /> : <AdminTable header={tableColumn} data={data} />}
+        </Grid>
+      </Container>
+    </React.Fragment>
   )
 }
