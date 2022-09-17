02842e467a3c6959e9f46a247727aa965d7a07e5
SOLBI1028 <hagkthfql@naver.com>
2022-09-17 14:35:20 +0900

    feat(fe): add signin page

--- frontend/src/screen/signIn/signIn.tsx
+++ frontend/src/screen/signIn/signIn.tsx
@@ -1,9 +1,42 @@
 import React from 'react'
+import { styled, OutlinedInputProps, TextField, Typography, Button, Box, Container } from '@mui/material'
+import { Colors } from '../../constant'
+import { useNavigate } from "react-router-dom"
 
-export default function signInScreen() {
+const TextFieldStyle = styled(TextField)({
+  '& .MuiFilledInput-root': {
+    backgroundColor: Colors.white,
+    boxShadow: `${Colors.primary_lighter} 0 0 2px 2px`,
+    borderRadius: 10,
+    overflow: 'hidden',
+    '&:hover': {
+      backgroundColor: Colors.white,
+      boxShadow: `${Colors.primary_lighter} 0 0 2px 2px`,
+    },
+  },
+})
+
+export default function SignInScreen() {
+  const navigate = useNavigate()
   return (
-    <div>
-      <h1>sign in detail</h1>
-    </div>
+    <React.Fragment>
+      <Container maxWidth="sm" sx={{height: '100vh', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center'}}>
+        <Typography variant="h5" sx={{color: Colors.primary, marginBottom: 5}}>Login</Typography>
+        <Box sx={{display: 'flex', flexDirection: 'column', width: '60%', marginBottom: 5}}>
+          <TextFieldStyle 
+            variant="filled"
+            label="이름"
+            style={{marginBottom: 8}}
+            InputProps={{ disableUnderline: true } as Partial<OutlinedInputProps>}
+          />
+          <TextFieldStyle 
+            variant="filled"
+            label="학번"
+            InputProps={{ disableUnderline: true } as Partial<OutlinedInputProps>}
+          />
+        </Box>
+        <Button onClick={()=>navigate('/')}>로그인</Button>
+      </Container>
+    </React.Fragment>
   )
 }
