6937ced79e4a5006daf790dc51339636e32e15d5
kig_laptop_2 <idmatthew9546@gmail.com>
2022-09-30 02:33:07 +0900

    chore(fe): set responsive adminTable

--- frontend/src/screen/admin/component/adminTable.tsx
+++ frontend/src/screen/admin/component/adminTable.tsx
@@ -2,11 +2,16 @@ import * as React from 'react'
 import Paper from '@mui/material/Paper'
 import Table from '@mui/material/Table'
 import TableBody from '@mui/material/TableBody'
-import TableCell from '@mui/material/TableCell'
+import TableCell, { TableCellProps } from '@mui/material/TableCell'
 import TableContainer from '@mui/material/TableContainer'
 import TableHead from '@mui/material/TableHead'
 import TablePagination from '@mui/material/TablePagination'
 import TableRow from '@mui/material/TableRow'
+import { styled } from '@mui/material/styles'
+
+const CustomTableCell = styled(TableCell)<TableCellProps>({
+  wordBreak: 'keep-all',
+})
 
 type headerType = {
   id: string
@@ -38,11 +43,11 @@ export default function AdminTable({ header, data }: AdminTableType) {
   return (
     <Paper sx={{ width: '100%' }}>
       <TableContainer>
-        <Table stickyHeader aria-label="sticky table">
+        <Table stickyHeader aria-label="sticky table" sx={{ minWidth: 450 }}>
           <TableHead>
             <TableRow>
               {header.map((h) => (
-                <TableCell key={h.id}> {h.label} </TableCell>
+                <CustomTableCell key={h.id}> {h.label} </CustomTableCell>
               ))}
             </TableRow>
           </TableHead>
@@ -53,16 +58,17 @@ export default function AdminTable({ header, data }: AdminTableType) {
                 <TableRow hover role="checkbox" key={rowId}>
                   {header.map((column) => {
                     if (column.id === 'index') {
-                      return <TableCell> {rowId}</TableCell>
+                      return <CustomTableCell> {rowId}</CustomTableCell>
                     } else {
                       const value = row[column.id]
                       if (column.id === 'role') {
-                        if (value === 0) return <TableCell>운영진</TableCell>
-                        if (value === 1) return <TableCell>운영기수</TableCell>
-                        if (value === 2) return <TableCell>활동기수</TableCell>
+                        if (value === 0) return <CustomTableCell>운영진</CustomTableCell>
+                        if (value === 1) return <CustomTableCell>운영기수</CustomTableCell>
+                        if (value === 2) return <CustomTableCell>활동기수</CustomTableCell>
                       } else if (column.id === 'campus')
-                        return <TableCell>{value === 'n' ? '자연과학' : '인문사회'}</TableCell>
-                      else return <TableCell>{String(value)}</TableCell>
+                        return <CustomTableCell>{value === 'n' ? '자연과학' : '인문사회'}</CustomTableCell>
+                      else if (column.id === 'register') return <CustomTableCell>{value ? 'O' : 'X'}</CustomTableCell>
+                      else return <CustomTableCell sx={{ wordBreak: 'keep-all' }}>{String(value)}</CustomTableCell>
                     }
                   })}
                 </TableRow>
