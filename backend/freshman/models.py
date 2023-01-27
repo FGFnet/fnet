cce14bc59b4317318933b164ae6dfc64b67482dc
k1g99 <idmatthew9546@gmail.com>
2023-01-27 23:17:32 +0900

    chore(be): add lc and freshman models and migrate

--- backend/freshman/models.py
+++ backend/freshman/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
+from django.db.models.fields import CharField, BooleanField
+from django.db.models.fields.related import ForeignKey
+from lc.models import LC
 
 # Create your models here.
+class Freshman(models.Model):
+    NATURAL_SCIENCE = 'NC'
+    ENGINEERING = 'EN'
+    SOCIAL_SCIENCE = 'SS'
+    HUMANITIES_SCIENCE = 'HS'
+    DEPARTMENT_CHOICES = [
+        (NATURAL_SCIENCE, 'nat'),
+        (ENGINEERING, 'eng'),
+        (SOCIAL_SCIENCE, 'soc'),
+        (HUMANITIES_SCIENCE, 'hum'),
+    ]
+
+    lc = ForeignKey(LC, on_delete=CASCADE, null=True)
+    name = CharField(max_length=30)
+    phone_number = CharField(max_length=13, null=True)
+    register = BooleanField(default=False)
+    department = CharField(max_length=10, choices=DEPARTMENT_CHOICES) # n(자연과학),e(공학),s(사회과학),h(인문사회) 
+
+    def __str__(self):
+        return self.name
--- backend/lc/models.py
+++ backend/lc/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
+from django.db.models.fields import CharField, DateField, IntegerField
+from django.db.models.fields.related import ForeignKey
+from fg.models import FG
 
 # Create your models here.
+class Schedule(models.Model):
+    date = DateField()
+    day = IntegerField()
+
+    def __str__(self):
+        return f'{self.date}'
+
+class LC(models.Model):
+    id = models.AutoField(primary_key=True)
+    fg_n_id = ForeignKey(FG, on_delete=CASCADE, null=True, related_name='fg_n')
+    fg_s_id = ForeignKey(FG, on_delete=CASCADE, null=True, related_name='fg_s')
+    schedule = IntegerField()
+    name = CharField(max_length=10)
+    total = IntegerField()
+    
+    class Meta:
+        db_table = 'lc'
+        ordering = ['schedule', 'name']
+    
+    def __str__(self):
+        return self.name
