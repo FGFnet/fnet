ac37e7b694c4730fcf05c27e85c41faa28f7195b
pinecone28 <hagkthfql@naver.com>
2023-02-03 18:00:37 +0900

    feat(be): add production setting

--- backend/fnet/settings.py
+++ backend/fnet/settings.py
@@ -11,7 +11,13 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-from .dev_settings import *
+from .get_env import get_env
+
+production_env = get_env("FNET_ENV", "dev") == "production"
+if production_env:
+    from .production_settings import *
+else:
+    from .dev_settings import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
