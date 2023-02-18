9c961a4db7a855b5e87b30cfb0073387c43916c8
k1g99 <idmatthew9546@gmail.com>
2023-02-18 19:18:14 +0900

    feat: use session storage in recoil

--- frontend/src/store/user.ts
+++ frontend/src/store/user.ts
@@ -2,16 +2,18 @@ import { atom } from 'recoil'
 import { recoilPersist } from 'recoil-persist'
 import { FG } from '../model'
 
-const {persistAtom} = recoilPersist()
+const { persistAtom } = recoilPersist({
+  storage: sessionStorage,
+})
 
-export const userState = atom<FG|null>({
+export const userState = atom<FG | null>({
   key: 'userState',
   default: null,
-  effects_UNSTABLE: [persistAtom]
+  effects_UNSTABLE: [persistAtom],
 })
 
 export const accesstoken = atom<string>({
   key: 'accesstoken',
   default: '',
-  effects_UNSTABLE: [persistAtom]
+  effects_UNSTABLE: [persistAtom],
 })
